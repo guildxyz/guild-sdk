bdef458164226efd37199e01151e340f648be449
Devidxyz <abt.devid@gmail.com>
2022-03-15 21:26:01 +0100

    Added types for guild and role crud

--- src/types.ts
+++ src/types.ts
@@ -36,46 +36,23 @@ type Theme = {
   backgroundCss?: any;
 };
 
-type Requirement = {
-  id: number;
-  type: string;
-  address: string;
-  chain: string;
-  roleId: number;
-  name: string;
-  symbol: string;
-  data: {
-    id?: string;
-    amount?: number;
-    strategy?: { name: string; params: any };
-    addresses?: string[];
-    attribute?: {
-      trait_type: string;
-      value?: string;
-      interval?: {
-        min: number;
-        max: number;
-      };
-    };
-  };
-};
+type Chain =
+  | "ETHEREUM"
+  | "BSC"
+  | "POLYGON"
+  | "XDAI"
+  | "AVALANCHE"
+  | "FANTOM"
+  | "ARBITRUM"
+  | "CELO"
+  | "HARMONY"
+  | "JUICEBOX"
+  | "GOERLI"
+  | "OPTIMISM"
+  | "MOONRIVER"
+  | "GNOSIS";
 
-type Role = {
-  id: number;
-  name: string;
-  description: string;
-  imageUrl: string;
-  logic: string;
-  requirements: Requirement[];
-  platforms: {
-    roleId: number;
-    platformId: number;
-    inviteChannel: string;
-    discordRoleId: string;
-  }[];
-  members: string[];
-  memberCount: number;
-};
+type Logic = "AND" | "OR" | "NAND" | "NOR";
 
 type GetGuildByIdResponse = {
   id: number;
@@ -92,7 +69,44 @@ type GetGuildByIdResponse = {
     type: string;
     platformName: string;
   }[];
-  roles: Role[];
+  roles: {
+    id: number;
+    name: string;
+    description: string;
+    imageUrl: string;
+    logic: Logic;
+    requirements: {
+      id: number;
+      type: string;
+      address: string;
+      chain: Chain;
+      roleId: number;
+      name: string;
+      symbol: string;
+      data: {
+        id?: string;
+        amount?: number;
+        strategy?: { name: string; params: any };
+        addresses?: string[];
+        attribute?: {
+          trait_type: string;
+          value?: string;
+          interval?: {
+            min: number;
+            max: number;
+          };
+        };
+      };
+    }[];
+    platforms: {
+      roleId: number;
+      platformId: number;
+      inviteChannel: string;
+      discordRoleId: string;
+    }[];
+    members: string[];
+    memberCount: number;
+  }[];
   owner: {
     id: number;
     address: string;
@@ -104,11 +118,153 @@ type GetUserAccessResponse = {
   access: boolean;
 }[];
 
+type Requirement =
+  | { type: "FREE" }
+  | {
+      type: "COIN";
+      chain: Chain;
+      data: {
+        amount: number;
+      };
+    }
+  | {
+      type: "ERC20";
+      chain: Chain;
+      address: string;
+      data: {
+        amount: number;
+      };
+    }
+  | {
+      type: "ERC721" | "ERC1155";
+      chain: Chain;
+      address: string;
+      data: {
+        id?: number;
+        amount: number;
+        attribute?:
+          | {
+              trait_type: string;
+              value: string;
+            }
+          | {
+              trait_type: string;
+              interval: { min: number; max: number };
+            };
+      };
+    }
+  | {
+      type: "POAP";
+      data: {
+        id: string;
+      };
+    }
+  | {
+      type: "MIRROR";
+      chain: Chain;
+      address: string;
+      data: {
+        id: number;
+      };
+    }
+  | {
+      type: "SNAPSHOT";
+      chain: Chain;
+      data: {
+        startegy: object;
+      };
+    }
+  | {
+      type: "JUICEBOX";
+      chain: Chain;
+      data: {
+        id: number;
+        amount: number;
+      };
+    }
+  | {
+      type: "WHITELIST";
+      data: {
+        addresses: string[];
+      };
+    };
+
+type Role = {
+  name: string;
+  imageUrl?: string;
+  description?: string;
+  logic: Logic;
+  requirements: Requirement[];
+};
+
+type CreateGuildParams = {
+  name: string;
+  imageUrl?: string;
+  description?: string;
+  roles: Role[];
+  theme?: Theme[];
+  showMembers?: boolean;
+};
+
+type UpdateGuildParams = {
+  name?: string;
+  imageUrl?: string;
+  description?: string;
+  roles?: Role[];
+  theme?: Theme[];
+  showMembers?: boolean;
+};
+
+type CreateGUildResponse = {
+  id: number;
+  name: string;
+  urlName: string;
+  description: string;
+  imageUrl: string;
+  createdAt: string;
+  showMembers: boolean;
+};
+
+// TODO UpdateGuildResponse
+
+type DeleteGuildResponse = {
+  success: boolean;
+};
+
+type CreateRoleParams = Role;
+
+type UpdateRoleParams = {
+  name?: string;
+  imageUrl?: string;
+  description?: string;
+  logic?: Logic;
+  requirements?: Requirement[];
+};
+
+type GetRoleResponse = Role;
+type CreateRoleResponse = Role;
+type UpdateRoleResponse = Role;
+type DeleteRoleResponse = {
+  success: boolean;
+};
+
 export {
+  Chain,
+  Requirement,
   GetMembershipsResponse,
   JoinResponse,
   RequestWithAuth,
   GetAllGuildsResponse,
   GetGuildByIdResponse,
   GetUserAccessResponse,
+  CreateGuildParams,
+  UpdateGuildParams,
+  CreateGUildResponse,
+  DeleteGuildResponse,
+  GetRoleResponse,
+  CreateRoleParams,
+  UpdateRoleParams,
+  CreateRoleResponse,
+  UpdateRoleResponse,
+  DeleteRoleResponse,
 };
