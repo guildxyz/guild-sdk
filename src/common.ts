e77b32a4c36f264d1282da40a090fb7bfd99eb18
Csete Balázs <csetebalu@gmail.com>
2023-10-05 11:25:27 +0200

    GUILD_SDK prefix for all env vars

--- src/common.ts
+++ src/common.ts
@@ -1,18 +1,18 @@
 import { consts } from "@guildxyz/types";
 
 const globals = {
-  apiBaseUrl: process.env.GUILD_BASE_URL ?? "https://api.guild.xyz/v2",
+  apiBaseUrl: process.env.GUILD_SDK_BASE_URL ?? "https://api.guild.xyz/v2",
   headers: {
     "Content-Type": "application/json",
     [consts.SDK_VERSION_HEADER_NAME]: "2.0.0-rc.2",
     [consts.SDK_PROJECT_NAME_HEADER_NAME]: "",
 
-    ...(process.env.PRIVELEGED_KEY
-      ? { [consts.AUTH_HEADER_NAME]: process.env.PRIVELEGED_KEY }
+    ...(process.env.GUILD_SDK_PRIVELEGED_KEY
+      ? { [consts.AUTH_HEADER_NAME]: process.env.GUILD_SDK_PRIVELEGED_KEY }
       : {}),
 
-    ...(process.env.PRIVELEGED_SERVICE_NAME
-      ? { [consts.SERVICE_HEADER_NAME]: process.env.PRIVELEGED_SERVICE_NAME }
+    ...(process.env.GUILD_SDK_SERVICE_NAME
+      ? { [consts.SERVICE_HEADER_NAME]: process.env.GUILD_SDK_SERVICE_NAME }
       : {}),
   },
 };
