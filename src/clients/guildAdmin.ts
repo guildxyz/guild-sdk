6eeadb6d4ab0d8acdd7da97fa1b46b83b1d2a698
Csete Balázs <csetebalu@gmail.com>
2023-12-08 13:25:13 +0100

    add create / update / delete

--- src/clients/guildAdmin.ts
+++ src/clients/guildAdmin.ts
@@ -1,5 +1,5 @@
-import { GuildAdmin } from "@guildxyz/types";
-import { callGuildAPI } from "../utils";
+import { GuildAdmin, Schemas } from "@guildxyz/types";
+import { SignerFunction, callGuildAPI } from "../utils";
 
 const guildAdmin = {
   get: (guildIdOrUrlName: string | number, userId: number) =>
@@ -12,6 +12,48 @@ const guildAdmin = {
     callGuildAPI<GuildAdmin[]>({
       url: `/guilds/${guildIdOrUrlName}/admins`,
       method: "GET",
+    }),
+
+  create: (
+    guildIdOrUrlName: string | number,
+    guildAdminCreationParams: Schemas["GuildAdminCreationPayload"],
+    signer: SignerFunction
+  ) =>
+    callGuildAPI<GuildAdmin>({
+      url: `/guilds/${guildIdOrUrlName}/admins`,
+      method: "POST",
+      body: {
+        schema: "GuildAdminCreationPayloadSchema",
+        data: guildAdminCreationParams,
+      },
+      signer,
+    }),
+
+  update: (
+    guildIdOrUrlName: string | number,
+    adminUserId: number,
+    guildAdminUpdateParams: Schemas["GuildAdminUpdatePayload"],
+    signer: SignerFunction
+  ) =>
+    callGuildAPI<GuildAdmin>({
+      url: `/guilds/${guildIdOrUrlName}/admins/${adminUserId}`,
+      method: "PUT",
+      body: {
+        schema: "GuildAdminUpdatePayloadSchema",
+        data: guildAdminUpdateParams,
+      },
+      signer,
+    }),
+
+  delete: (
+    guildIdOrUrlName: string | number,
+    adminUserId: number,
+    signer: SignerFunction
+  ) =>
+    callGuildAPI<void>({
+      url: `/guilds/${guildIdOrUrlName}/admins/${adminUserId}`,
+      method: "DELETE",
+      signer,
     }),
 };
 
